ab8944c3092080afc84e7369eb009800ac42a69d
MuthuPandi-B <muthup61@gmail.com>
2024-11-23 15:31:46 +0530

    deployed link updated

--- src/Services/api.js
+++ src/Services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api =axios.create ({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://petadoption-backend-73o2.onrender.com',
 
 });
 api.interceptors.request.use(
